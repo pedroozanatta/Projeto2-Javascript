d2ff409d9c822a20d531485652bad6cc9cbd6d11
Pedro Zanatta <pedrohenriquezanattadeoliveira@gmail.com>
2025-07-02 11:40:16 -0300

    .

--- script.js
+++ script.js
@@ -35,8 +35,8 @@ function jogoCalculoMedia() {
     let soma = 0;
 
     for (let i = 1; i <= quantidade; i++) {
-    let numero = parseFloat(prompt(`Digite o ${i}º número:`));
-    soma += numero;
+        let numero = parseFloat(prompt(`Digite o ${i}º número:`));
+        soma += numero;
     }
 
     let media = soma / quantidade;
